67b2ebec318a33645d94a0d194b684932322f4e7
Ridzzz0Alam <ridwan0gts@gmail.com>
2026-09-03 23:28:37 +0200

    feat:added the theme context

--- src/lib/theme-context.ts
+++ src/lib/theme-context.ts
@@ -1,15 +1,18 @@
-import { createContext } from "react";
+import { createContext } from 'react';
 
-export type Theme = "dark" | "light" | "system";
+
+export type Theme = 'dark' | 'light' | 'system';
+
 
 export type ThemeProviderState = {
-  theme: Theme;
-  setTheme: (theme: Theme) => void;
-  resolvedTheme: "light" | "dark";
+ theme: Theme;
+ setTheme: (theme: Theme) => void;
+ resolvedTheme: 'light' | 'dark';
 };
 
+
 export const ThemeProviderContext = createContext<ThemeProviderState>({
-  theme: "system",
-  setTheme: () => null,
-  resolvedTheme: "dark",
+ theme: 'system',
+ setTheme: () => null,
+ resolvedTheme: 'dark',
 });
